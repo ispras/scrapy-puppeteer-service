2b236f827f0127b7ef96dc570ab4ab2a9c5afcbe
Max Varlamov <max2103@yandex.ru>
2019-09-03 22:16:11 +0300

    added wait option for scroll

--- routes/scroll.js
+++ routes/scroll.js
@@ -2,17 +2,29 @@ const express = require('express');
 const utils = require('../helpers/utils');
 const router = express.Router();
 
+const DEFAULT_TIMEOUT = 1000;  // 1 second
 
 async function action(page, request) {
-    await page.hover(request.body.selector);
+    await Promise.all([
+        page.waitFor(request.body.waitOptions.selectorOrTimeout || DEFAULT_TIMEOUT),
+        page.hover(request.body.selector),
+    ]);
     return utils.formResponse(page, request.query.closePage);
 }
 
-// Method that scrolls page to a certain selector
+// Method that scrolls page to a certain selector.
+// Example body:
+//  body = {
+//     "selector": "", //<string> A selector to search for element to scroll
+//     "waitOptions": {
+//         // if selectorOrFunctionOrTimeout is a string, then the first argument is treated as a selector or xpath, depending on whether or not it starts with '//', and the method is a shortcut for page.waitForSelector or page.waitForXPath
+//         // if selectorOrFunctionOrTimeout is a number, then the first argument is treated as a timeout in milliseconds and the method returns a promise which resolves after the timeout
+//         "selectorOrTimeout":...,
+//     }
+//  }
 router.post('/', async function (req, res, next) {
     try {
         let response = await utils.perfomAction(req, action);
-
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(response));
     } catch (e) {
