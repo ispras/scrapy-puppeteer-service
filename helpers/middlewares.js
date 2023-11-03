e82dbb885b8c0dad6361676a355c0691f4aa03ef
matthew <matvey.zvancov10@gmail.com>
2023-11-03 17:53:42 +0300

    Added processing of accidental context losses

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -7,14 +7,35 @@ exports.exceptionMiddleware = async function exceptionMiddleware(err, req, res, 
     const pageId = err.pageId || req.query.pageId;
     const errorMessage = err.message || 'Unknown error';
 
-    res.status(500);
+
     if (contextId) {
         res.header('scrapy-puppeteer-service-context-id', contextId);
+    }  // What if contextId is undefined? Maybe we should skip such situations...
+
+    if (err.contextId) {  // there was a context, but something went wrong
+        res.status(500);
+
+        res.send({
+            contextId,
+            pageId,
+            error: errorMessage
+        });
+    } else {  // No context. Possibly, our service was restarted
+        res.status(422);
+
+        res.send({
+            error: errorMessage
+        });
     }
+
+    next(err);
+}
+
+exports.noContextMiddleware = async function noContextMiddleware(err, req, res, next) {
+    res.status(422);
+
     res.send({
-        contextId,
-        pageId,
-        error: errorMessage
+        error: "no context"
     });
     next(err);
 }
