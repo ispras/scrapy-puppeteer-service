cc6ff31ccab783922a943f55c29d0e67d52c5100
Matthew <matvey.zvancov10@gmail.com>
2023-12-08 15:19:34 +0300

    DeadContext handling (#39)
    * Added processing of accidental context losses
    
    * Deleted extra middleware
    
    * Added custom errors for more flexible development.
    Now we response with not only 422 but also with 500
    
    * Major logic of middleware:
    set status and send response

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -1,3 +1,5 @@
+const exceptions = require("./exceptions");
+
 exports.exceptionMiddleware = async function exceptionMiddleware(err, req, res, next) {
     if (res.headersSent) {
         return next(err);
@@ -7,14 +9,25 @@ exports.exceptionMiddleware = async function exceptionMiddleware(err, req, res, 
     const pageId = err.pageId || req.query.pageId;
     const errorMessage = err.message || 'Unknown error';
 
-    res.status(500);
     if (contextId) {
         res.header('scrapy-puppeteer-service-context-id', contextId);
     }
+
+    if (err.contextId) {  // there was a context, but something went wrong
+        res.status(500);
+    } else {  // No context. Possibly, our service was restarted
+        if (err instanceof exceptions.PageNotFoundError || err instanceof exceptions.ContextNotFoundError) {
+            res.status(422);
+        } else {
+            res.status(500);
+        }
+    }
+
     res.send({
         contextId,
         pageId,
         error: errorMessage
     });
+
     next(err);
 }
