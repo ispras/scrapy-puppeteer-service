8764ff1ddb9d2665ce5ee3ce739ad76248b1e71a
matthew <matvey.zvancov10@gmail.com>
2023-12-08 15:14:23 +0300

    Major logic of middleware: set status and send response

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -9,30 +9,25 @@ exports.exceptionMiddleware = async function exceptionMiddleware(err, req, res, 
     const pageId = err.pageId || req.query.pageId;
     const errorMessage = err.message || 'Unknown error';
 
-
     if (contextId) {
         res.header('scrapy-puppeteer-service-context-id', contextId);
     }
 
     if (err.contextId) {  // there was a context, but something went wrong
         res.status(500);
-
-        res.send({
-            contextId,
-            pageId,
-            error: errorMessage
-        });
     } else {  // No context. Possibly, our service was restarted
         if (err instanceof exceptions.PageNotFoundError || err instanceof exceptions.ContextNotFoundError) {
             res.status(422);
         } else {
             res.status(500);
         }
-
-        res.send({
-            error: errorMessage
-        });
     }
+
+    res.send({
+        contextId,
+        pageId,
+        error: errorMessage
+    });
 
     next(err);
 }
