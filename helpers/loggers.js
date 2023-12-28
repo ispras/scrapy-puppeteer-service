1b3f3d27d2141302add0190361bca58ae267b876
matthew <matvey.zvancov10@gmail.com>
2023-12-28 12:01:12 +0300

    Name consistency

--- helpers/loggers.js
+++ helpers/loggers.js
@@ -1,33 +1,33 @@
 const winston = require('winston');
 
-const log_printer = winston.format.printf((info) => {
+const logPrinter = winston.format.printf((info) => {
     let log = `[${info.timestamp}] ${info.level}: ${info.message}`;
     log = info.stack ? `${log}\n${info.stack}\n` : log;
     return log;
 });
 
-const file_format = winston.format.combine(
+const fileFormat = winston.format.combine(
     winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
     winston.format.errors({stack: true}),
-    log_printer,
+    logPrinter,
 );
 
-const console_format = winston.format.combine(
+const consoleFormat = winston.format.combine(
     winston.format.errors({stack: true}),
     winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
     winston.format.colorize({level: true}),
-    log_printer,
+    logPrinter,
 );
 
 const transports = [
     new winston.transports.Console({
         level: 'http',
-        format: console_format,
+        format: consoleFormat,
     }),
     new winston.transports.File({
         level: 'http',
         filename: 'logs.log',
-        format: file_format,
+        format: fileFormat,
     }),
 ]
 
