913c4391972cd19d5eca24b20babebb6a0c85ea6
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2024-08-12 13:07:18 +0300

    add documentation

--- routes/form_action.js
+++ routes/form_action.js
@@ -20,6 +20,15 @@ async function action(page, request) {
 
 }
 
+// body = {
+//     "inputMapping": { A dictionary where each key is a CSS selector, and each value is another dictionary containing details about the input for that element:
+//         "selector": <string> The CSS selector for the input element (used as the key).
+//         "value": <string> The text to be inputted into the element.
+//         "delay": <number> A delay (in milliseconds) between each keystroke when inputting the text. Defaults to 0 if not provided.
+//     },
+//     "submitButton": <string> The CSS selector for the form's submit button. If provided, the button will be clicked after filling in the form.
+// }
+//
 router.post('/', async function (req, res, next) {
 
     try {
