f117b8ef5add504be33fd49140a999ba84c2ab40
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2024-08-14 11:45:07 +0300

    add some check in fill_form

--- routes/fill_form.js
+++ routes/fill_form.js
@@ -31,6 +31,13 @@ async function action(page, request) {
 //
 router.post('/', async function (req, res, next) {
 
+    if (!req.body.inputMapping) {
+        res.status(400);
+        res.send("No inputMapping provided in fill_form request");
+        next();
+        return;
+    }
+
     try {
         let response = await utils.performAction(req, action);
         res.header('scrapy-puppeteer-service-context-id', response.contextId);
