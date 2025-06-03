eec3d2ef38e302c4046406b7afe51c6ba8154765
matthew <matvey.zvancov10@gmail.com>
2025-06-03 17:36:36 +0300

    route

---
+++ routes/cloudflare_captcha_solver.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const {cloudflareCaptchaSolver} = require('../actions/cloudflare_captcha_solver');
+const utils = require('../helpers/utils');
+
+router.post('/', async function (req, res, next) {
+    if (!process.env.TOKEN_2CAPTCHA) {
+        res.status(501);
+        res.send("TOKEN_2CAPTCHA is not provided!");
+        next();
+        return;
+    }
+
+    try {
+        let response = await utils.performAction(req, cloudflareCaptchaSolver);
+        res.header('scrapy-puppeteer-service-context-id', response.contextId);
+        res.send(response)
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
